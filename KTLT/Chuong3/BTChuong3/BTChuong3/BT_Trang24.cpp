89c639d51b5cd13732c574f5d450e39971bfea6e
hoangthinh1306 <hthinh650@gmail.com>
2024-08-11 22:08:07 +0700

    BuiHoangThinh: finish Chuong3 Bai 6 Trang 24

---
+++ KTLT/Chuong3/BTChuong3/BTChuong3/BT_Trang24.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Hàm cắt chuỗi họ tên thành chuỗi họ lót và chuỗi tên
+void splitName(const string& fullName, string& middleName, string& lastName) {
+    size_t lastSpace = fullName.find_last_of(' ');
+
+    // Nếu không tìm thấy khoảng trắng, tức là chỉ có tên
+    if (lastSpace == string::npos) {
+        middleName = "";
+        lastName = fullName;
+    }
+    else {
+        middleName = fullName.substr(0, lastSpace);
+        lastName = fullName.substr(lastSpace + 1);
+    }
+
+    // Xóa khoảng trắng trong chuỗi họ lót
+    middleName.erase(remove(middleName.begin(), middleName.end(), ' '), middleName.end());
+}
+
+int main() {
+    string fullName = "Nguyen Van A";
+    string middleName, lastName;
+
+    // Cắt chuỗi họ tên
+    splitName(fullName, middleName, lastName);
+
+    // In kết quả
+    cout << "Chuoi ho lot: " << middleName << endl;
+    cout << "Chuoi ten: " << lastName << endl;
+
+    return 0;
+}
